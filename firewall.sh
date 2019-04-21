0b04746d02ed1075c132a98c8224e9b655a4ead8
peter-z-wang <peter.wang@colorado.edu>
2019-04-21 14:38:01 -0600

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -10,12 +10,22 @@ iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type time-exceeded -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type destination-unreachable -j ACCEPT
 
-iptables -A INPUT -p tcp -s 100.64.0.0/16 --dport 22 -j ACCEPT
-iptables -A INPUT -p tcp -s 10.21.32.0/24 --dport 22 -j ACCEPT
-iptables -A INPUT -p tcp -s 198.18.0.0/16 --dport 22 -j ACCEPT
+if ["$1" != "E"]
+	then
+		iptables -A INPUT -p tcp -s 100.64.0.0/16 --dport 22 -j ACCEPT
+		iptables -A INPUT -p tcp -s 10.21.32.0/24 --dport 22 -j ACCEPT
+		iptables -A INPUT -p tcp -s 198.18.0.0/16 --dport 22 -j ACCEPT
+fi
 
 iptables -P INPUT DROP
-iptables -P FORWARD ACCEPT
+
+if ["$1" == "A"]
+	then
+		iptables -P FORWARD ACCEPT
+
+else
+	iptables -P FORWARD DROP
+fi
 
 service iptables save
 
